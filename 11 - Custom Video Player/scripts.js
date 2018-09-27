3a5daa92fd1f3fa7ccd5e1455f52b05328f27065
Beto Costa <robertomarx@gmail.com>
2018-09-27 18:30:21 -0300

    11 - custom video player - done

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -22,13 +22,53 @@ function updateButton() {
 }
 
 function skip() {
-    console.log('Skipping!');
+  // console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+//   console.log(this.value);
+}
+
+function handleProgress() {
+    // Get video time in percentage to apply the flex value in the progress bar
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+// control the progress bar
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(e)
 }
 
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
+video.addEventListener('play', displayTime);
 video.addEventListener('pause', updateButton);
+// Handle the progress bar
+video.addEventListener('timeupdate', handleProgress);
 
+// buttons
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+progressBar.addEventListener('change', handleProgress);
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+function displayTime() {
+    const time = document.getElementsByClassName('time')[0];
+    const totalTime = document.getElementsByClassName('totalTime')[0];
+    time.innerText = parseFloat(video.currentTime);
+    totalTime.innerText = parseFloat(video.duration) * 0.0166667;
+    setTimeout(displayTime, 1000);
+}
+
